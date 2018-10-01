8f3e5bc6f3338829f89bc615aeb1a72738885746
DESKTOP-6RROJVT\omizu <mizuki.kawasaki.likeit@gmail.com>
2018-10-01 16:02:20 +0900

    1-7

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,8 +21,14 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
+		bo = true;
+		by = 10 ;
+		s = 20 ;
+		c = 'あ';
+		i = 300 ;
+		l = 1234;
+		f = 1.9F ;
+		d = 2.7 ;
 
 
 
@@ -31,7 +37,14 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
